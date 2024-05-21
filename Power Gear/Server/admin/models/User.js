6119d88a43cf478e5766a8f854ea2bc1fdc7e771
bacem-slimi <slimii.bacem@gmail.com>
2024-05-22 00:36:50 +0100

    B

---
+++ Power Gear/Server/admin/models/User.js
@@ -0,0 +1,27 @@
+
+
+const { sequelize,DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Order.belongsTo(User); 
+User.hasMany(Order);
+
+module.exports = User;
